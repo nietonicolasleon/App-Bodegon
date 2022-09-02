92575bcd8085c7ba32a3f1c7939c558a8d13ae12
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-09-02 12:00:15 -0300

    Created some objects in main class to test the program. Works fine.

--- src/bodegon/Bodegon.java
+++ src/bodegon/Bodegon.java
@@ -2,6 +2,17 @@ package bodegon;
 
 public class Bodegon {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Comida c = new Comida("Comida de Tres Pasos", 2500);
+        Bebida b1 = new Bebida("Vino San Felipe Malbec Roble", 1000, "750 ml.");
+        Mesa mesa1 = new Mesa(5, 7500);
+        mesa1.AgregarComida(c);
+        mesa1.AgregarComida(c);
+        mesa1.AgregarComida(c);
+        mesa1.AgregarComida(c);
+        mesa1.AgregarComida(c);
+        mesa1.AgregarBebida(b1);
+        mesa1.AgregarBebida(b1);
+        mesa1.AgregarBebida(b1);
+        mesa1.DetalleCuenta();
     }
 }
