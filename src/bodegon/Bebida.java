131fdfedb7542f9931e0aa999e9cdf167284e562
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-09-02 10:26:04 -0300

    Added atributes and methods to Bebida Class

--- src/bodegon/Bebida.java
+++ src/bodegon/Bebida.java
@@ -1,5 +1,37 @@
 package bodegon;
 
 public class Bebida {
-    
+    private String nombre;
+    private double precio;
+    private String contenido;
+
+    public Bebida(String n, double p, String c) {
+        this.nombre = n;
+        this.precio = p;
+        this.contenido = c;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
 }
