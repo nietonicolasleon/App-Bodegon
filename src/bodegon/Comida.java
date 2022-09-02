0a87c7d095af0e9f37c9b9a6bc9fc7f1ade075e0
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-09-02 10:17:46 -0300

    Added atributes and methods to Comida Class

--- src/bodegon/Comida.java
+++ src/bodegon/Comida.java
@@ -1,5 +1,27 @@
 package bodegon;
 
 public class Comida {
+    private String nombre;
+    private double precio;
     
+    public Comida(String n, double p){
+        this.nombre = n;
+        this.precio = p;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
