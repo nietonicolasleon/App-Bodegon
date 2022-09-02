71320870ba2d7f48149f5a88178b1eeb473c3e54
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-09-02 09:59:37 -0300

    Created the classes that we are gonna use

---
+++ src/bodegon/Bodegon.java
@@ -0,0 +1,7 @@
+package bodegon;
+
+public class Bodegon {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/bodegon/Mesa.java
@@ -0,0 +1,5 @@
+package bodegon;
+
+public class Mesa {
+    
+}
